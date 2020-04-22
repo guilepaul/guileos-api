642b5766af5ef35fc47333f23579edf6396304a4
Guilherme Di Paula <guilepaul@gmail.com>
2020-04-22 19:38:41 -0300

    db config

--- src/main/java/com/guiledev/guileos/api/controller/ClienteController.java
+++ src/main/java/com/guiledev/guileos/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.guiledev.guileos.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,22 +13,13 @@ import com.guiledev.guileos.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Guilherme Di Paula");
-		cliente1.setEmail("guilepaul@gmail.com");
-		cliente1.setTelefone("95530-3615");
-		
-		
-		var cliente2 =new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Luanda Oliveira");
-		cliente2.setEmail("luanda230282@gmail.com");
-		cliente2.setTelefone("97127-5114");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 
 }
