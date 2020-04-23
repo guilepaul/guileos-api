22a5d6568789b1ece5cf4aaf2f45cdea33f626a4
Guilherme Di Paula <guilepaul@gmail.com>
2020-04-23 14:30:01 -0300

    Get method

--- src/main/java/com/guiledev/guileos/domain/repository/ClienteRepository.java
+++ src/main/java/com/guiledev/guileos/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.guiledev.guileos.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import com.guiledev.guileos.domain.model.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+	List<Cliente> findByNome(String nome);
 }
